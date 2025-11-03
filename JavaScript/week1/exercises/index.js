3e80a497ceb8062e36c8c1a205d2be77af2a62b0
PalomaCardozo <palomacardozo88@gmail.com>
2025-11-03 17:43:32 +0100

    Add comments to exercises

--- JavaScript/week1/exercises/index.js
+++ JavaScript/week1/exercises/index.js
@@ -1,132 +1,46 @@
-// Types
-console.log(typeof 3);
-console.log(typeof -33);
-console.log(typeof "3");
-const threeConst = 3;
-console.log(threeConst);
-let threeLet = 3;
-console.log(threeLet);
-console.log(typeof 'console.log("console.log(console.log(""))")');
-console.log(typeof typeof 45);
-const names = ["benjamin", "Christopher"];
-console.log(typeof names[0]);
-console.log(typeof [3][0]);
-console.log(typeof true);
+console.log("Hello World");
 
-/* console.log("Hello World");
+//first exercise: Create a variable that is 24 times 55
 
-// Strings
-let message = "123";
-message = "Hello HackYourFuture";
-console.log(message);
-
-// Numbers
-const pi = 3.14;
-const piFloat = 3.1416;
-console.log(pi);
-console.log(piFloat);
-
-// Booleans
-const isActive = true;
-const isInactive = false;
-console.log(isActive);
-console.log(isInactive);
-
-// Objects
-const person = {
-  name: "Paloma",
-  age: 37,
-  isStudent: true,
-};
-console.log(person.name);
-console.log(person.nationality);
-console.log(person);
-
-// Arrays
-const fruits = ["Apple", "Banana", "Cherry"];
-const vegetables = new Array("Carrot", "Broccoli", "Spinach");
-console.log(fruits[0]);
-console.log(fruits);
-console.log(vegetables[1]);
-console.log(vegetables);
-console.log(vegetables[5]);
-
-console.log(fruits.length);
-
-// Types
-console.log(typeof 3); // number
-console.log(typeof -33); // number
-console.log(typeof "3"); // string
-const threeConst = 3; // create and assign
-console.log(threeConst); // print 3
-let threeLet = 3; // create and assign
-console.log(threeLet); // print 3
-console.log(typeof 'console.log("console.log(console.log(""))")'); // string
-console.log(typeof typeof 45); // string
-const names = ["benjamin", "Christopher"]; // array of strings
-console.log(typeof names[0]); // string
-console.log([3, 2, 1][2]); // print 1
-console.log(typeof true); // boolean
-const emptyObject = []; // empty array
-console.log(typeof emptyObject); // object */
-
-/* const array = [1, 2, 3];
-array[5] = 6;
-console.log(array);
-
-const object = {
-  name: "Paloma",
-};
-object.age = 37;
-object.name = "Paloma Cardozo";
-console.log("My name is", object.name);
-console.log(object); */
-
-/* console.log(typeof null); // object
-console.log(typeof undefined); // undefined */
-
-/*
-//first exercise
 const exercise1 = 24 * 55;
 console.log(exercise1);
 
-//second exercise
+//second exercise: Create a const and set it to be equal to your name
+
 const myName = "Paloma";
+console.log(myName);
+
 const exercise2 = "Paloma" == myName;
 console.log(exercise2);
 
-//exercise 3
+//exercise 3: With javascript console.log the first character in your name
+
 console.log(myName[0]);
 
-//exercise 4
+//exercise 4: Create an array with 3 strings, three numbers and three booleans
+
 const newArray = [
-  "Paloma",
-  "Matilde",
-  "Carlos",
-  37,
-  11,
-  58 / 9,
+  "Paloma: ",
+  "She has a daughter, Matilde,",
+  "who is 11 years old.",
+  2 * 80,
+  11 / 3,
+  58 % 9,
   1 === 2,
   9 >= 8,
-  true,
+  56984 !== 56984,
 ];
+
 console.log(newArray);
 
-//exercise 5
+for (const element of newArray) {
+  console.log(typeof element);
+}
+
+//exercise 5: console.log the 4. element in the array made above
+
 console.log(newArray[3]);
 
-//exercise 6
+//exercise 6: With javascript console.log the last character in your name.
+
 console.log(myName[myName.length - 1]);
-*/
-
-/* let name1 = "benjamin";
-name1 = "benjamin-better";
-console.log(name1);
-
-const pizzaPrice = 78;
-const pizzaPriceDiscounted = pizzaPrice - 10;
-console.log(pizzaPriceDiscounted);
-
-const users = ["peter", "Johnny", "Børge", "Paloma"];
-const lastUser = users[users.length - 1];
-console.log(lastUser); */
