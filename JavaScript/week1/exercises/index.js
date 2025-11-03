3a7ebc90f193733ba234c167e7d196873920344e
PalomaCardozo <palomacardozo88@gmail.com>
2025-11-03 17:24:03 +0100

    Add basic html and js

---
+++ JavaScript/week1/exercises/index.js
@@ -0,0 +1,132 @@
+// Types
+console.log(typeof 3);
+console.log(typeof -33);
+console.log(typeof "3");
+const threeConst = 3;
+console.log(threeConst);
+let threeLet = 3;
+console.log(threeLet);
+console.log(typeof 'console.log("console.log(console.log(""))")');
+console.log(typeof typeof 45);
+const names = ["benjamin", "Christopher"];
+console.log(typeof names[0]);
+console.log(typeof [3][0]);
+console.log(typeof true);
+
+/* console.log("Hello World");
+
+// Strings
+let message = "123";
+message = "Hello HackYourFuture";
+console.log(message);
+
+// Numbers
+const pi = 3.14;
+const piFloat = 3.1416;
+console.log(pi);
+console.log(piFloat);
+
+// Booleans
+const isActive = true;
+const isInactive = false;
+console.log(isActive);
+console.log(isInactive);
+
+// Objects
+const person = {
+  name: "Paloma",
+  age: 37,
+  isStudent: true,
+};
+console.log(person.name);
+console.log(person.nationality);
+console.log(person);
+
+// Arrays
+const fruits = ["Apple", "Banana", "Cherry"];
+const vegetables = new Array("Carrot", "Broccoli", "Spinach");
+console.log(fruits[0]);
+console.log(fruits);
+console.log(vegetables[1]);
+console.log(vegetables);
+console.log(vegetables[5]);
+
+console.log(fruits.length);
+
+// Types
+console.log(typeof 3); // number
+console.log(typeof -33); // number
+console.log(typeof "3"); // string
+const threeConst = 3; // create and assign
+console.log(threeConst); // print 3
+let threeLet = 3; // create and assign
+console.log(threeLet); // print 3
+console.log(typeof 'console.log("console.log(console.log(""))")'); // string
+console.log(typeof typeof 45); // string
+const names = ["benjamin", "Christopher"]; // array of strings
+console.log(typeof names[0]); // string
+console.log([3, 2, 1][2]); // print 1
+console.log(typeof true); // boolean
+const emptyObject = []; // empty array
+console.log(typeof emptyObject); // object */
+
+/* const array = [1, 2, 3];
+array[5] = 6;
+console.log(array);
+
+const object = {
+  name: "Paloma",
+};
+object.age = 37;
+object.name = "Paloma Cardozo";
+console.log("My name is", object.name);
+console.log(object); */
+
+/* console.log(typeof null); // object
+console.log(typeof undefined); // undefined */
+
+/*
+//first exercise
+const exercise1 = 24 * 55;
+console.log(exercise1);
+
+//second exercise
+const myName = "Paloma";
+const exercise2 = "Paloma" == myName;
+console.log(exercise2);
+
+//exercise 3
+console.log(myName[0]);
+
+//exercise 4
+const newArray = [
+  "Paloma",
+  "Matilde",
+  "Carlos",
+  37,
+  11,
+  58 / 9,
+  1 === 2,
+  9 >= 8,
+  true,
+];
+console.log(newArray);
+
+//exercise 5
+console.log(newArray[3]);
+
+//exercise 6
+console.log(myName[myName.length - 1]);
+*/
+
+/* let name1 = "benjamin";
+name1 = "benjamin-better";
+console.log(name1);
+
+const pizzaPrice = 78;
+const pizzaPriceDiscounted = pizzaPrice - 10;
+console.log(pizzaPriceDiscounted);
+
+const users = ["peter", "Johnny", "Børge", "Paloma"];
+const lastUser = users[users.length - 1];
+console.log(lastUser); */
